7da991b150682ab1c8598d608ceb06213db8fa19
cstrick-WE <cstrickland@wilburellis.com>
2023-01-24 02:14:41 -0600

    Add header of page

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-app-header',
+  selector: 'app-header',
   templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss']
+  styleUrls: ['./app-header.component.scss'],
 })
-export class AppHeaderComponent {
-
-}
+export class AppHeaderComponent {}
